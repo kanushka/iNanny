2f3518b03a545b06d5cf3e0e35021b2b53983882
Kanushka Gayan <kanushkanet@gmail.com>
2018-09-19 23:59:42 +0530

    add new error msg view

--- application/views/errors/html/error_general.php
+++ application/views/errors/html/error_general.php
@@ -1,64 +1,54 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>Error</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<title>Error</title>
+	<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.css" media="screen,projection" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<style type="text/css">
+		.primary-btn-color {
+			background-color: #388e3c;
+		}
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+		.primary-btn-color:hover {
+			background-color: #2e7d32;
+		}
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+		p {
+			font-size: 18px;
+			margin-top: 60px;
+			color: #4A4A4A;
+			text-align: center;
+		}
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+	</style>
 </head>
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+
+
+	<div class="row" style="margin-bottom: 0px;">
+		<div class="col s12" style="height: 50vh;  background-image: radial-gradient(50% 100%, #4caf50,50%, #388e3c 69%, #2e7d32 100%); display: flex; align-items: center; justify-content: center; flex-direction: column;">
+			<p style="font-size: 120px; color: #fff; margin-top: 10px; margin-bottom: 0px;">ERORR</p>
+			<p style="font-size: 24px; color: #fff; margin-top: 10px; text-align:center;">
+				<?php echo $heading; ?>
+			</p>
+		</div>
+	</div>
+	<div class="row" style="margin-bottom: 0px;">
+		<div class="col s12 m12 l10 offset-l1 xl6 offset-xl3" style="height: 50vh; display: flex; align-items: center;  flex-direction: column;">
+			<?php echo $message; ?>
+			<p style="font-size: 18px; margin-top: 60px; color: #4A4A4A; text-align: center;">Not to worry you can go back to
+				our home page by clicking the below button</p>
+			<a class="waves-effect waves-light btn-large primary-btn-color" href="<?php echo base_url(); ?>">Back to home page</a>
+		</div>
 	</div>
 </body>
+
+
 </html>
